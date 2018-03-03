210ec4b3d0113badc49163f7dce19f3f725bd6ef
michaelryan <michaeldryanii>
2018-03-03 13:23:53 -0500

    added Fib with "dynamic" programming commit 6

--- venv/fibonacci.py
+++ venv/fibonacci.py
@@ -21,6 +21,8 @@ def calc_fib(place):
 	return fib_nums[len(fib_nums)-1]
 
 
-x = calc_fib(20)
-x1 = fib_nums[19]
-print('Calc fib to 20th place is {} and the index of 19 val is {} '.format(x, x1))
+x_val_pass = 1000
+x = calc_fib(x_val_pass)
+x1 = fib_nums[(x_val_pass-1)]
+print('Calc fib to {}th place is {} and the index of {} val is {} '.format(x_val_pass, x, (x_val_pass-1), x1))
+print(x == x1)
