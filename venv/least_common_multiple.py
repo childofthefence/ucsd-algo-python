f8f1ceaf549d85d313152793b926dc248f80e006
michaelryan <michaeldryanii>
2018-03-04 18:07:34 -0500

    least common multiple, i've been sick and forgot what i was doing.  commit # something.  Need to find a way to put the version into here as a vaariable value.

--- venv/least_common_multiple.py
+++ venv/least_common_multiple.py
@@ -1,7 +1,7 @@
 # calculate the smallest number divisible both by a and b (params passed to the function)
 
 
-def least_common_shared_factor(a, b):
+def least_common_multiple(a, b):
 	
 	base_val = 0
 	least_common_start = 0
@@ -33,4 +33,13 @@ def least_common_shared_factor(a, b):
 			
 			
 # some tests
-
+print least_common_multiple(4, 3)
+print least_common_multiple(15, 6)
+print least_common_multiple(21, 4)
+print least_common_multiple(33, 9)
+print least_common_multiple(104, 27)
+print least_common_multiple(191, 15)
+print least_common_multiple(231, 92)
+print least_common_multiple(4, 7)
+print least_common_multiple(5, 9)
+print least_common_multiple(0, 1)
